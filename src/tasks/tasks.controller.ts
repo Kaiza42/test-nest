49338082547bb8b773a62efa93815412adc491ba
Emerick <decdu59@live.fr>
2025-04-28 22:52:53 +0200

    feat(tasks): implement task controller with CRUD routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,4 +2,36 @@ import { Controller, Get, Post, Delete, Patch, Param, Body } from '@nestjs/commo
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) { }
+
+    @Get()
+    getALLTasks() {
+        return this.tasksService.findAll();
+    }
+
+    @Post()
+    createTasks(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ) {
+        return this.tasksService.createTask(title, description);
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string) {
+        this.tasksService.deleteTask(+id);
+        return { message: `Tâche ${id} suprimée.` };
+    }
+
+
+    @Patch(':id')
+    updateTask(
+        @Param('id') id: string,
+        @Body('title') title: string,
+        @Body('description') description: string,
+        @Body('status') status: string,
+    ) {
+        return this.tasksService.updateTask(+id, title, description, status);
+    }
+}
